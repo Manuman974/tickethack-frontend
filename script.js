fa445daaaec3225805f9e661f6de4b0d063cd34a
Manu <manuelmanrique6@hotmail.com>
2024-07-03 17:11:01 +0200

    mise a jour frontend

--- script.js
+++ script.js
@@ -15,12 +15,29 @@ document.querySelector('#searchButton').addEventListener('click', function () {
     .then(response => response.json())
     .then(data => {
         if (data.result) {
+        const Blankwindowright = document.querySelector("#windowright")
+        Blankwindowright.innerHTML = ""
+    
+    }
+
+        if (data.result) {
             // Afficher les trajets//
+
+                for (let i=0; i<data.trips.length; i++) { 
+                    const departure = data.trips[i].departure
+                    const arrival = data.trips[i].arrival
+                    const hour = new Date(data.trips[i].date).getHours()
+                    const minute = new Date(data.trips[i].date).getMinutes()
+                    const price = data.trips[i].price
+               
             document.querySelector("#windowright").innerHTML += `
-            <div>${departure > arrival}</div>
-            `
+               <div id="triplist"> ${departure} > ${arrival} ${hour}:${minute} ${price}€
+                <button class="bookButton">Book</button>
+            </div>`
         }
-      console.log(data.trips);
+    }
+    })
+    //   console.log(data.trips);
     });
        console.log('Click detected!')
 
@@ -29,8 +46,7 @@ document.querySelector('#searchButton').addEventListener('click', function () {
 //     const textToCompare = document.querySelector('#departureInput').value.toLowerCase();
 //     if (document.querySelectorAll('#departureInput').textContent.toLowerCase().includes(textToCompare) === false) {
 // }
-}
-);
+
 
 // .then(res => res.json())
 // .then(reponseDuBAck => {
